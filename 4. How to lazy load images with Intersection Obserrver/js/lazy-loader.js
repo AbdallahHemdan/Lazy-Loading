1c9e6adfb020c968b2110f9abec8ed4bc2da5e40
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-30 19:36:47 +0200

    Add How to lazy load images with Intersection Obserrver

---
+++ 4. How to lazy load images with Intersection Obserrver/js/lazy-loader.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll('[data-src]');
+
+const imagesOption = {
+  root: null,
+  threshold: 0,
+  rootMargin: '0px',
+};
+
+const preloadImage = element => {
+  const src = element.getAttribute('data-src');
+
+  if (src) {
+    element.src = src;
+  }
+};
+
+const imagesObserver = new IntersectionObserver((entries, imagesObserver) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      preloadImage(entry.target);
+      imagesObserver.unobserve(entry.target);
+    }
+  });
+}, imagesOption);
+
+images.forEach(image => {
+  imagesObserver.observe(image);
+});
